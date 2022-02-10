d4a8f73272d889b83ba75f3f23d989688ce4c709
NasVa <aska.vakulchik@mail.ru>
2022-02-10 23:58:12 +0300

    added dependency class

---
+++ DependencyInjectionContainer/Dependency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionContainer
+{
+    class Dependency
+    {
+        public int lifeTime;
+
+        public Type interfaceType { get; }
+
+        public Type implementationType { get; }
+
+        public object instance;
+
+        public Dependency(int lifeTime, Type interfaceType, Type implementationType)
+        {
+            this.lifeTime = lifeTime;
+            this.interfaceType = interfaceType;
+            this.implementationType = implementationType;
+            instance = null;
+        }
+    }
+}
